51471777fa2792ef379738817b3a9774754bf4d1
Kkibe <137826199+Kkibe@users.noreply.github.com>
2023-12-12 11:53:55 +0300

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,15 +3,15 @@ const Post = ({post}) => {
     return (
         <a href={post.url} className='post' title='post'>
             <div className="image-container">
-                <img src={post.urlToImage} alt="" />
+                <img src={post.image_url} alt="" />
             </div>
             <h2>{post.title}</h2>
             <div className="content">
                 {post.description}
             </div>
             <div className="info">
-                <span>{post.author ? post.author : post.source.name}</span>
-                <span>{new Date().toDateString(post.publishedAt)}</span>
+                <span>{post.creator ? post.creator[0] : post.source_id}</span>
+                <span>{new Date().toDateString(post.pubDate)}</span>
             </div>
         </a>
     );
